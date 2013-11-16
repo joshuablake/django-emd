a0a7e6a8cf446e3ef29c68e4332ab0ba81e22c0e
Joshua Blake <joshbblake@gmail.com>
2013-11-16 16:25:01 +0000

    Move py files to subdir and add a setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name = 'django-emd',
+      version = '0.1.0',
+      description = 'Django models for prices from EMD',
+      author = 'Joshua Blake',
+      author_email = 'joshbblake@gmail.com',
+      packages = ['prices'],
+)
