8b40c8f2737cf96611038d5e7b22d2c634f739fb
Joshua Blake <joshbblake@gmail.com>
2013-11-20 19:31:26 +0000

    Include all packages

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name = 'django-emd',
       version = '0.1.0',
       description = 'Django models for prices from EMD',
       author = 'Joshua Blake',
       author_email = 'joshbblake@gmail.com',
-      packages = ['prices'],
+      packages = find_packages(),
 )
